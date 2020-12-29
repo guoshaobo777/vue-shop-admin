3d974fcf857dba799b1c39dc227f198edd3ba656
gsb <1932938921@qq.com>
2020-12-29 16:50:42 +0800

    完成商品管理模块的开发

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,11 @@ const routes = [
       { path: '/Welcome', component: () => import('../components/Welcome.vue') },
       { path: '/users', component: () => import('../components/user/Users.vue') },
       { path: '/rights', component: () => import('../components/power/Rights.vue') },
-      { path: '/roles', component: () => import('../components/power/Roles.vue') }
+      { path: '/roles', component: () => import('../components/power/Roles.vue') },
+      { path: '/categories', component: () => import('../components/goods/Cate.vue') },
+      { path: '/params', component: () => import('../components/goods/Params.vue') },
+      { path: '/goods', component: () => import('../components/goods/List.vue') },
+      { path: '/goods/add', component: () => import('../components/goods/Add.vue') }
     ]
   }
 ]
